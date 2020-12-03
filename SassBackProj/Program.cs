6f3d6b861179cd10cdcb295193901b123118f601
AdamSzr <adam.szr98@gmail.com>
2020-12-03 15:39:10 +0100

    Clear Websocket

--- SassBackProj/Program.cs
+++ SassBackProj/Program.cs
@@ -13,8 +13,6 @@ namespace SassBackProj
     {
         public static void Main(string[] args)
         {
-            
-            
             CreateHostBuilder(args).Build().Run();
         }
 
--- SassBackProj/Startup.cs
+++ SassBackProj/Startup.cs
@@ -6,14 +6,14 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.FileProviders;
 using System.IO;
 using SassBackProj.Middleware;
+using Microsoft.AspNetCore.Http.Features;
 using SassBackProj.Services.FoodService;
-using MongoDB.Driver;
-using Microsoft.AspNetCore.Http;
 
 namespace SassBackProj
 {
@@ -33,9 +33,7 @@ namespace SassBackProj
             //int x = 0;
             //Task.Run(() => { x = 2; invokeAsync() });
 
-
-
-
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -89,42 +87,12 @@ namespace SassBackProj
 
 
 
+
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/mango", async context =>
-                {
-                    IMongoDatabase db;
-                    var client = new MongoDB.Driver.MongoClient();
-                    db = client.GetDatabase("SASS_Project");
-                    string tabName = "users";
-                    var records = db.GetCollection<user>(tabName);
-
-                    await context.Response.WriteAsync(System.Text.Json.JsonSerializer.Serialize(records, records.GetType()));
-                });
                 endpoints.MapControllers();
             });
         }
-    }
-    class user
-    {
-        public string email { get; set; }
-        public string gender { get; set; }
-        public string phone_number { get; set; }
-        public string birthdate { get; set; }
-        public location location { get; set; }
-        public string username { get; set; }
-        public string password { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string title { get; set; }
-        public string picture { get; set; }
-
-    }
-    class location
-    {
-        public string street { get; set; }
-        public string city { get; set; }
-        public string state { get; set; }
-        public int postcode { get; set; }
     }
 }
